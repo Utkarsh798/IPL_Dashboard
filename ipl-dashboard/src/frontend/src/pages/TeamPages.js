fa623eda0b4c662445e9781402bfad2c20707956
Utkarsh798 <utkarshamin98@gmail.com>
2024-02-23 12:12:23 -0600

    React App with Team Page, Match Detail Card, Match Small Card

--- ipl-dashboard/src/frontend/src/pages/TeamPages.js
+++ ipl-dashboard/src/frontend/src/pages/TeamPages.js
@@ -1,22 +1,21 @@
 import {React, useEffect, useState} from 'react';
+import {useParams} from "react-router-dom";
 import {MatchDetailCard} from "../component/MatchDetailCard";
 import {MatchSmallCard} from "../component/MatchSmallCard";
 
 export const TeamPage = () => {
 
     const [team, setTeam] = useState({matches: []});
+    const {teamName} = useParams();
     useEffect(
         () => {
             const fetchTeam = async () => {
-                const response = await fetch('http://localhost:8080/team/Mumbai%20Indians');
+                const response = await fetch(`http://localhost:8080/team/${teamName}`);
                 const data = await response.json();
                 setTeam(data);
             };
             fetchTeam();
-
-
-
-        }, []
+        }, [teamName]
     );
 
 
@@ -28,8 +27,8 @@ export const TeamPage = () => {
     return (
         <div className="TeamPage">
             <h1>{team.teamName}</h1>
-            <MatchDetailCard match = {team.matches[0]}/>
-            {team.matches.slice(1).map(match => <MatchSmallCard match={match} />)}
+            <MatchDetailCard teamName = {team.teamName} match = {team.matches[0]}/>
+            {team.matches.slice(1).map(match => <MatchSmallCard teamName = {team.teamName} match={match} />)}
 
         </div>
     );
