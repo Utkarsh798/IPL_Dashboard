8b64c7cb36a149956c38915903a111c2d7dafcb3
Utkarsh798 <utkarshamin98@gmail.com>
2024-02-23 13:27:56 -0600

    Added Frontend Match Page

--- ipl-dashboard/src/frontend/src/pages/MatchPage.js
+++ ipl-dashboard/src/frontend/src/pages/MatchPage.js
@@ -1,10 +1,28 @@
-import {React} from 'react';
+import {React, useEffect, useState} from 'react';
+import {useParams} from "react-router-dom";
+import {MatchDetailCard} from "../component/MatchDetailCard";
 
 export const MatchPage = () => {
+
+    const [matches, setMatch] = useState([]);
+    const {teamName, year} = useParams();
+    useEffect(
+        () => {
+            const fetchTeam = async () => {
+                const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+                const data = await response.json();
+                setMatch(data);
+            };
+            fetchTeam();
+        }, []
+    );
 
     return (
         <div className="TeamPage">
            <h1>Match Page</h1>
+            {
+                matches.map(match => <MatchDetailCard teamName = {teamName} match={match} />)
+            }
         </div>
     );
 }
